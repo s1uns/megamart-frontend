14ed31b4117f1d6ce63f16da9f52fc7aa1633fe3
Illia Teliuk <illiateliuk@gmail.com>
2024-01-08 23:39:51 +0200

    Moved GoodBlock to index.jsx for easier importing, created special Skeleton placeholder for the good block

---
+++ src/components/GoodBlock/Skeleton.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import ContentLoader from "react-content-loader";
+
+const Skeleton = () => (
+    <ContentLoader
+        className="good-block"
+        speed={2}
+        width={280}
+        height={440}
+        viewBox="0 0 280 440"
+        backgroundColor="#b2a4a4"
+        foregroundColor="#2d2a2a"
+    >
+        <rect x="156" y="306" rx="0" ry="0" width="14" height="0" />
+        <rect x="1" y="0" rx="5" ry="5" width="280" height="280" />
+        <rect x="0" y="363" rx="10" ry="10" width="93" height="30" />
+        <rect x="0" y="300" rx="10" ry="10" width="280" height="35" />
+        <rect x="138" y="349" rx="19" ry="19" width="140" height="45" />
+        <rect x="0" y="405" rx="11" ry="11" width="276" height="24" />
+    </ContentLoader>
+);
+
+export default Skeleton;
