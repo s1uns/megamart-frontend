e84c04d4bc18121590a1b3ee9e477e1951889db5
Illia Teliuk <illiateliuk@gmail.com>
2024-01-15 21:47:56 +0200

    Added context to the application and Search component

--- src/App.jsx
+++ src/App.jsx
@@ -3,19 +3,26 @@ import Header from "./components/Header";
 import Home from "./pages/Home";
 import Cart from "./pages/Cart";
 import NotFound from "./pages/NotFound";
-import {Switch, Route, Link, Routes} from "react-router-dom"
+import { Route, Routes } from "react-router-dom";
+import { createContext, useState } from "react";
+
+export const SearchContext = createContext("");
 
 function App() {
+    const [searchValue, setSearchValue] = useState("");
+
     return (
         <div className="wrapper">
-            <Header />
-            <div className="content">
+            <SearchContext.Provider value={{ searchValue, setSearchValue }}>
+                <Header />
+                <div className="content">
                     <Routes>
                         <Route path="/" element={<Home />} />
                         <Route path="/cart" element={<Cart />} />
                         <Route path="*" element={<NotFound />} />
                     </Routes>
-            </div>
+                </div>
+            </SearchContext.Provider>
         </div>
     );
 }
--- src/components/Search/index.jsx
+++ src/components/Search/index.jsx
@@ -1,7 +1,10 @@
-import React from "react";
+import React, { useContext } from "react";
 import styles from "./Search.module.scss";
+import { SearchContext } from "../../App";
 
 export const Search = () => {
+    const { searchValue, setSearchValue } = useContext(SearchContext);
+    
     return (
         <div className={styles.root}>
             <svg
@@ -58,7 +61,28 @@ export const Search = () => {
                     />
                 </g>
             </svg>
-            <input className={styles.input} placeholder="Enter the good..." />
+            <input
+                value={searchValue}
+                onChange={(event) => setSearchValue(event.target.value)}
+                className={styles.input}
+                placeholder="Enter the good..."
+            />
+            {searchValue && (
+                <svg
+                    fill="none"
+                    height="24"
+                    viewBox="0 0 24 24"
+                    width="24"
+                    xmlns="http://www.w3.org/2000/svg"
+                    className={styles.clear}
+                    onClick={() => setSearchValue("")}
+                >
+                    <path
+                        d="M6.2253 4.81108C5.83477 4.42056 5.20161 4.42056 4.81108 4.81108C4.42056 5.20161 4.42056 5.83477 4.81108 6.2253L10.5858 12L4.81114 17.7747C4.42062 18.1652 4.42062 18.7984 4.81114 19.1889C5.20167 19.5794 5.83483 19.5794 6.22535 19.1889L12 13.4142L17.7747 19.1889C18.1652 19.5794 18.7984 19.5794 19.1889 19.1889C19.5794 18.7984 19.5794 18.1652 19.1889 17.7747L13.4142 12L19.189 6.2253C19.5795 5.83477 19.5795 5.20161 19.189 4.81108C18.7985 4.42056 18.1653 4.42056 17.7748 4.81108L12 10.5858L6.2253 4.81108Z"
+                        fill="currentColor"
+                    />
+                </svg>
+            )}
         </div>
     );
 };
