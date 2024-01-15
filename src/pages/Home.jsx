09496dd8d2ea7432ee5fc1d19b25699845aa052a
Illia Teliuk <illiateliuk@gmail.com>
2024-01-15 21:48:30 +0200

    Added pagination and context to the application

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,12 @@
-import React from "react";
+import React, { useContext } from "react";
 import Categories from "../components/Categories";
 import Sort from "../components/Sort";
 import GoodBlock from "../components/GoodBlock";
 import { useState, useEffect } from "react";
 import Skeleton from "../components/GoodBlock/Skeleton";
+import ReactPaginate from "react-paginate";
+import Pagination from "../components/Pagination";
+import { SearchContext } from "../App";
 
 const Home = () => {
     const [items, setItems] = useState([]);
@@ -14,21 +17,34 @@ const Home = () => {
         sortProperty: "rating",
     });
     const [sortOrder, setSortOrder] = useState(true);
+    const [currentPage, setCurrentPage] = useState(0);
+    const [totalPages, setTotalPages] = useState(1);
+    const { searchValue } = useContext(SearchContext);
+
 
     useEffect(() => {
         setIsLoading(true);
         fetch(
-            "https://localhost:7295/api/goods/list?category=" +
-                categoryId +
-                `&sortBy=${sortType.sortProperty}&order=${sortOrder}`
+            `https://localhost:7295/api/goods/list?page=${currentPage}&limit=5&category=${categoryId}&sortBy=${sortType.sortProperty}&order=${sortOrder}&search=${searchValue}`
         )
             .then((response) => response.json())
             .then((data) => {
-                setItems(data);
+                if(data.length < 0){
+                    setCurrentPage(0);
+                }
+
+                setItems(data.data);
+                setTotalPages(data.totalPages);
                 setIsLoading(false);
             });
         window.scrollTo(0, 0);
-    }, [categoryId, sortType.sortProperty, sortOrder]);
+    }, [
+        categoryId,
+        sortType.sortProperty,
+        sortOrder,
+        searchValue,
+        currentPage,
+    ]);
 
     return (
         <div className="container">
@@ -63,6 +79,10 @@ const Home = () => {
                           />
                       ))}
             </div>
+            <Pagination
+                onChangePage={(page) => setCurrentPage(page)}
+                totalPages={totalPages}
+            />
         </div>
     );
 };
