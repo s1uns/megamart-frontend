6b58d25550889718a733a0bb5050582eadecd829
Illia Teliuk <illiateliuk@gmail.com>
2024-01-16 23:08:32 +0200

    Updated the onClear method so that now it keeps the focur on the search field

--- src/components/Search/index.jsx
+++ src/components/Search/index.jsx
@@ -1,10 +1,15 @@
-import React, { useContext } from "react";
+import React, { useContext, useRef } from "react";
 import styles from "./Search.module.scss";
 import { SearchContext } from "../../App";
 
 export const Search = () => {
     const { searchValue, setSearchValue } = useContext(SearchContext);
-    
+    const inputRef = useRef();
+
+    const onClear = () => {
+        setSearchValue("");
+        inputRef.current.focus();
+    };
     return (
         <div className={styles.root}>
             <svg
@@ -62,6 +67,7 @@ export const Search = () => {
                 </g>
             </svg>
             <input
+                ref={inputRef}
                 value={searchValue}
                 onChange={(event) => setSearchValue(event.target.value)}
                 className={styles.input}
@@ -75,7 +81,7 @@ export const Search = () => {
                     width="24"
                     xmlns="http://www.w3.org/2000/svg"
                     className={styles.clear}
-                    onClick={() => setSearchValue("")}
+                    onClick={() => onClear()}
                 >
                     <path
                         d="M6.2253 4.81108C5.83477 4.42056 5.20161 4.42056 4.81108 4.81108C4.42056 5.20161 4.42056 5.83477 4.81108 6.2253L10.5858 12L4.81114 17.7747C4.42062 18.1652 4.42062 18.7984 4.81114 19.1889C5.20167 19.5794 5.83483 19.5794 6.22535 19.1889L12 13.4142L17.7747 19.1889C18.1652 19.5794 18.7984 19.5794 19.1889 19.1889C19.5794 18.7984 19.5794 18.1652 19.1889 17.7747L13.4142 12L19.189 6.2253C19.5795 5.83477 19.5795 5.20161 19.189 4.81108C18.7985 4.42056 18.1653 4.42056 17.7748 4.81108L12 10.5858L6.2253 4.81108Z"
